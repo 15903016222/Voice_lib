d2ccd84b2c370e925bf92ddf5cddbce6425ffc0c
Jake Yang <yanghuanjie@cndoppler.cn>
2017-08-17 14:18:28 +0800

    更新: Source测试工具

--- test_source/test_source.cpp
+++ test_source/test_source.cpp
@@ -1,50 +1,28 @@
 #include "test_source.h"
 
-#include "source.h"
 #include <QDebug>
 
 TestSource::TestSource(QObject *parent) :
     QObject(parent),
-    m_beams1(new DplSource::Beams(parent)),
-    m_beams2(new DplSource::Beams(parent))
+    m_source(DplSource::Source::instance())
 {
-    m_beams1->set_beam_qty(2);
-    m_beams1->set_point_qty(605);
+    m_source->register_group(0, 1, 605);
 
-    m_beams2->set_beam_qty(2);
-    m_beams2->set_point_qty(605);
-
-    connect(m_beams1, SIGNAL(data_event()), this, SLOT(do_beamgrp1_data_event()));
-    connect(m_beams2, SIGNAL(data_event()), this, SLOT(do_beamgrp2_data_event()));
+    connect(m_source, SIGNAL(data_event()),
+            this, SLOT(do_data_event()),
+            Qt::DirectConnection);
 }
 
 void TestSource::start()
 {
     DplSource::Source *src = DplSource::Source::instance();
-    src->set_interval(1);
     src->start();
 }
 
-void TestSource::do_beamgrp1_data_event()
+void TestSource::do_data_event()
 {
-    qDebug()<<"BeamGroup 1";
-    DplSource::BeamPointer beamPtr;
-    for (int i = 0; i < m_beams1->beam_qty(); ++i) {
-        beamPtr = m_beams1->get(i);
-        show_measure(beamPtr);
-    }
-    qDebug("\n");
-}
-
-void TestSource::do_beamgrp2_data_event()
-{
-    qDebug()<<"BeamGroup 2";
-    DplSource::BeamPointer beamPtr;
-    for (int i = 0; i < m_beams2->beam_qty(); ++i) {
-        beamPtr = m_beams2->get(i);
-        show_measure(beamPtr);
-    }
-    qDebug("\n");
+    DplSource::BeamsPointer beamsPtr = m_source->current_beams(0);
+    show_measure(beamsPtr->get(0));
 }
 
 void TestSource::show_measure(const DplSource::BeamPointer &beamPtr)
@@ -54,8 +32,14 @@ void TestSource::show_measure(const DplSource::BeamPointer &beamPtr)
         return;
     }
     qDebug()<<"Beam Index:"<<beamPtr->index();
-    qDebug("Gate A Height(%f) Position(%d)", beamPtr->gate_a_height()/20.47, beamPtr->gate_a_position());
-    qDebug("Gate B Height(%f) Position(%d)", beamPtr->gate_b_height()/20.47, beamPtr->gate_b_position());
-    qDebug("Gate I Height(%f) Position(%d)", beamPtr->gate_i_height()/20.47, beamPtr->gate_i_position());
+    qDebug("Gate A Height(%f) Position(%d)",
+           beamPtr->gate_peak(DplSource::Beam::GATE_A)/20.47,
+           beamPtr->gate_peak_position(DplSource::Beam::GATE_A));
+    qDebug("Gate B Height(%f) Position(%d)",
+           beamPtr->gate_peak(DplSource::Beam::GATE_B)/20.47,
+           beamPtr->gate_peak_position(DplSource::Beam::GATE_B));
+    qDebug("Gate I Height(%f) Position(%d)",
+           beamPtr->gate_peak(DplSource::Beam::GATE_I)/20.47,
+           beamPtr->gate_peak_position(DplSource::Beam::GATE_I));
     qDebug("Encoder x:y(%d, %d)", beamPtr->encoder_x(), beamPtr->encoder_y());
 }
--- test_source/test_source.h
+++ test_source/test_source.h
@@ -3,7 +3,7 @@
 
 #include <QObject>
 
-#include "beams.h"
+#include "source.h"
 
 class TestSource : public QObject
 {
@@ -15,14 +15,12 @@ public:
 signals:
 
 public slots:
-    void do_beamgrp1_data_event();
-    void do_beamgrp2_data_event();
+    void do_data_event();
 
 private:
-    DplSource::Beams *m_beams1;
-    DplSource::Beams *m_beams2;
-
     void show_measure(const DplSource::BeamPointer &beam);
+
+    DplSource::Source *m_source;
 };
 
 #endif // __TEST_SOURCE_H__
